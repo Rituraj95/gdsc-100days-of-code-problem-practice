aa2774efc9da4f51df5c6d21647cb7788e1b41bb
Rituraj95 <96946078+Rituraj95@users.noreply.github.com>
2023-11-27 18:31:47 +0530

    Create even_odd.js

---
+++ even_odd.js
@@ -0,0 +1,12 @@
+
+let number = prompt("Enter a number:");
+
+
+number = parseInt(number);
+
+
+if (number % 2 === 0) {
+  console.log(`${number} is an even number.`);
+} else {
+  console.log(`${number} is an odd number.`);
+}
